ab0d0ad63c4a9fc5ece2f2a99b84067ffd8327d8
Jramovechi <jonathanramovechi@gmail.com>
2022-02-23 19:50:37 -0300

    Desafio 10-1

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // COMPONENTS
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-
-//FireBase - FireStore
-import { db } from "./firebase/firebaseConfig";
+import CategoryNav from "./components/CategoryNav/CategoryNav";
 
 // VIEWS
 import Shop from "./views/Shop/Shop";
 import About from "./views/About/About";
 import Detail from "./views/Detail/Detail";
 import CartView from "./views/CartView/CartView";
+import CategoryType from "./views/CategoryType/CategoryType";
 
 const App = () => {
   return (
@@ -27,11 +26,13 @@ const App = () => {
         <div className="App">
           <Header />
           <Nav />
+          <CategoryNav />
           <Routes>
             <Route path="/" element={<Shop />} />
             <Route path="/about" element={<About />} />
             <Route path="/cart" element={<CartView />} />
             <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/category/:id" element={<CategoryType />} />
           </Routes>
         </div>
       </Router>
--- src/components/CategoryNav/CategoryNav.js
+++ src/components/CategoryNav/CategoryNav.js
@@ -1,90 +1,24 @@
-//Import React
-import * as React from "react";
-
-//Import LINK
 import { Link } from "react-router-dom";
 
-//Import MUI
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+const CategoryType = () => {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 4 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+    <nav className="Navigation">
+      <ul className="List-ul">
+        <Link to="/CategoryType" className="Link">
+          Procesador
+        </Link>
+        <Link to="/CategoryType" className="Link">
+          Motherboard
+        </Link>
+        <Link to="/CategoryType" className="Link">
+          Gabinete
+        </Link>
+        <Link to="/CategoryType" className="Link">
+          PCs
+        </Link>
+      </ul>
+    </nav>
   );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
 };
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <Link to="/category/procesador" style={styles.link}>
-          PROCESADOR
-        </Link>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Link to="/category/motherboard" style={styles.link}>
-          MOTHERBOARD
-        </Link>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Link to="/category/gabinete" style={styles.link}>
-          GABINETE
-        </Link>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <Link to="/category/pc" style={styles.link}>
-          PC
-        </Link>
-      </TabPanel>
-    </Box>
-  );
-}
+export default CategoryType;
